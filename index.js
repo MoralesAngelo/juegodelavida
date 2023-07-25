1c52117d35426fea67e8db8f48cde7fc0c089c18
angelo morales <angelotuba3@gmail.com>
2023-07-25 18:39:20 +0200

    juegofuncionando

---
+++ index.js
@@ -0,0 +1,95 @@
+let arrays = [
+    [1, 1, 0], 
+    [1, 0, 1], 
+    [1, 0, 0]
+]
+
+let newArrays = [ [], [], [] ]
+
+const arrayCounts = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            countNeighbors(array, i, j)  
+        }
+        
+    }
+}
+
+const countNeighbors = (array, column, row) => {
+    let neighBors = 0
+
+    for (let i = column -1;  i < column + 2 && i < array.length; i++) {
+        if (array[i] === undefined) {
+            i++
+        }
+
+
+        for (let j = row -1;  j < row + 2; j++) {
+
+            if (array[i][j] === undefined) {
+                j++
+            }
+
+            if (array[i][j] === 1) {
+                neighBors++
+            }
+
+
+        }
+
+        
+    }    
+    
+    if (array[column][row] === 1) {
+            neighBors--
+        }
+    
+    liveOrDead(neighBors, column, row);
+};
+
+
+
+
+
+const liveOrDead = (neighBors, column, row) => {
+
+
+
+        if (neighBors < 2 || neighBors > 3) {
+            newArrays[column][row]=0;
+        }
+    
+        if (neighBors > 1 && neighBors < 4) {
+            newArrays[column][row] = 1;
+        }
+        return;
+}
+
+        
+        
+            
+
+
+
+
+
+const playGame = () => {
+    console.table(arrays);
+    arrayCounts(arrays);
+    arrays = newArrays;
+    newArrays = [ [0,0,0],[0,0,0],[0,0,0] ];
+
+
+}
+
+
+setInterval(() => {
+    playGame()
+}, 2000);
+
+
+
+
+
+
+
---
+++ prueba.js
@@ -0,0 +1,11 @@
+let arrays = [
+    [0, 1, 0], 
+    [1, 0, 1], 
+    [1, 0, 0]
+]
+
+console.log(arrays[0].length);
+
+
+//definir cuando vive y vuando muere
+//actualizar tablero con lo anterior 
